8722927b55a586e927857930d6633a5eef60cc91
devinlinux <ssmith969898@gmail.com>
2024-03-15 18:53:16 -0400

    first commit

---
+++ src/main/java/com/michaelb/clc/App.java
@@ -0,0 +1,13 @@
+package com.michaelb.clc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
