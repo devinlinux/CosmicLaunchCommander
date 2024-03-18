f59762bea3b48a43a58f691d41e3b930734ad75d
devinlinux <ssmith969898@gmail.com>
2024-03-18 00:01:36 -0400

    Made main init graphics

--- src/main/java/com/michaelb/clc/Main.java
+++ src/main/java/com/michaelb/clc/Main.java
@@ -6,18 +6,9 @@ import com.michaelb.clc.math.geom.*;
 
 public interface Main {
     static void main(String... args) {
-        /*
         initLogger();
         SwingUtilities.invokeLater(() ->
             new com.michaelb.clc.gui.Frame());
-        */
-        Shape tank = new Cylinder(5, 20);
-        Shape taper = new TruncatedCone(5, 3, 2);
-        Shape upperTank = new Cylinder(3, 10);
-        Shape noseCone = new Cone(3, 2);
-        Figure3D craft = new Figure3D(tank, taper, upperTank, noseCone);
-        System.out.println(craft.volume());
-        System.out.println(craft.surfaceArea());
     }
 
     private static void initLogger() {
