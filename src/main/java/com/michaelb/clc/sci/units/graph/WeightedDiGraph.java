383ff7377cd0ea2a7975e5d7ea33ecf92bd21cb9
devinlinux <ssmith969898@gmail.com>
2024-03-20 11:56:07 -0400

    Defined WeightedDiGraph

--- src/main/java/com/michaelb/clc/sci/units/graph/WeightedDiGraph.java
+++ src/main/java/com/michaelb/clc/sci/units/graph/WeightedDiGraph.java
@@ -1,12 +1,23 @@
 package com.michaelb.clc.sci.units.graph;
 
 //  imports
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class WeightedDiGraph<T> {
 
-    private List<Node<T>> nodes;
+    private final List<Node<T>> nodes;
+    private int numNodes;
 
-    public record Node<T>(T label, List<WeightedDiEdge<T>> edges) {}
-    public record WeightedDiEdge<T>(Node<T> to, double weight) {}
+    public WeightedDiGraph(T... labels) {
+        this.nodes = Arrays.stream(labels)
+            .map(label -> new Node<T>(label, List.of()))
+            .toList();
+    }
+
+    public static final record Node<T>(T label, List<WeightedDiEdge<T>> edges) {}
+    public static final record WeightedDiEdge<T>(Node<T> to, double weight) {}
+
+    public abstract List<WeightedDiEdge<T>> computePathBetween(T from, T to);
+    public abstract List<Double> computeWeightsBetween(T from, T to);
 }
