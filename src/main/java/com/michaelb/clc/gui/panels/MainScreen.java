f507ac8b1606feecad42030bf5a4c27136ffdb22
devinlinux <ssmith969898@gmail.com>
2024-03-23 16:23:48 -0400

    Added some setup to the main screen

--- src/main/java/com/michaelb/clc/gui/panels/MainScreen.java
+++ src/main/java/com/michaelb/clc/gui/panels/MainScreen.java
@@ -2,14 +2,41 @@ package com.michaelb.clc.gui.panels;
 
 //  imports
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.michaelb.clc.gui.Frame;
 
 public class MainScreen extends JPanel {
 
     private static final Color BACKGROUND_COLOR = Color.BLACK;
+
+    private Frame context;
+
+    public MainScreen(Frame context) {
+        this.context = context;
+        this.configurePanel();
+    }
+
+    private void configurePanel() {
+        this.setBorder(new EmptyBorder(10, 10, 10, 10));
+        this.setLayout(new GridBagLayout());
+    }
+
+    private void addButtons() {
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+    }
 
     @Override
     public void paintComponent(Graphics g) {
