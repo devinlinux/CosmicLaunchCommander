31369a09bbc9dd22c68e92e96182a3ddd1bab5d2
devinlinux <ssmith969898@gmail.com>
2024-03-24 00:01:02 -0400

    Finished update method

--- src/main/java/com/michaelb/clc/gui/panels/main/Star.java
+++ src/main/java/com/michaelb/clc/gui/panels/main/Star.java
@@ -1,5 +1,10 @@
 package com.michaelb.clc.gui.panels.main;
 
+//  imports
+import java.util.Random;
+
+import java.awt.Graphics;
+
 public class Star {
 
     private final int x;
@@ -11,6 +16,8 @@ public class Star {
     private int fadeStep = 0;
     private boolean fading;
 
+    private Random rand;
+
     public Star(final int x, final int y, final int size, final int fadeDuration) {
         this.x = x;
         this.y = y;
@@ -20,11 +27,21 @@ public class Star {
         this.fadeDuration = fadeDuration;
         this.fadeStep = 0;
         this.fading = false;
+
+        this.rand = new Random();
     }
 
     public void update() {
         if (fading) {
             fadeStep++;
+            if (fadeStep >= fadeDuration) {
+                fading = false;
+                fadeStep = 0;
+            }
+        } else {
+            if (rand.nextInt(100) < 2) {
+                fading = true;
+            }
         }
     }
 }
