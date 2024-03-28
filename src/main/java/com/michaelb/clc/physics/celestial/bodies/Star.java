f02a6de3013ce56a0d6e3b9dcbcd9858db8e81c7
devinlinux <ssmith969898@gmail.com>
2024-03-28 17:58:11 -0400

    Star stub class

---
+++ src/main/java/com/michaelb/clc/physics/celestial/bodies/Star.java
@@ -0,0 +1,8 @@
+package com.michaelb.clc.physics.celestial.bodies;
+
+public class Star extends CelestialBody {
+
+    public Star(final String name, final double mass, final double radius, final double x, final double y, final double z, final double velocity) {
+        super(name, mass, radius, x, y, z, velocity);
+    }
+}
