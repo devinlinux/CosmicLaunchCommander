cfcd91c06245c47d77e0349b1284a19cb489b07d
devinlinux <ssmith969898@gmail.com>
2024-03-28 17:21:37 -0400

    Added members to the star system

--- src/main/java/com/michaelb/clc/physics/celestial/StarSystem.java
+++ src/main/java/com/michaelb/clc/physics/celestial/StarSystem.java
@@ -1,12 +1,21 @@
 package com.michaelb.clc.physics.celestial;
 
+//  imports
+import java.util.List;
+import java.util.Arrays;
+
+import com.michaelb.clc.physics.celestial.bodies.CelestialBody;
+
 public final class StarSystem {
 
     private final String name;
+    private final List<CelestialBody> members;
 
-    public StarSystem(final String name) {
+    public StarSystem(final String name, CelestialBody... members) {
         this.name = name;
+        this.members = Arrays.asList(members);
     }
 
     public String name() { return this.name; }
+    public List<CelestialBody> members() { return this.members; }
 }
