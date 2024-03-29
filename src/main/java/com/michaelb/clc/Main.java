cb260d7bc1ce011764abdf2c79dcc5a71cda4179
devinlinux <ssmith969898@gmail.com>
2024-03-29 19:43:12 -0400

    Cleaned up Main

--- src/main/java/com/michaelb/clc/Main.java
+++ src/main/java/com/michaelb/clc/Main.java
@@ -1,12 +1,9 @@
 package com.michaelb.clc;
-import static com.michaelb.clc.math.MathUtil.*;
+
 public interface Main {
     static void main(String... args) {
-        Cartesian3D a = new Cartesian3D(1, 1, 1);
-        System.out.println(a.toSpherical());
-        System.out.println(a.toSpherical().toCartesian3D());
-//        initLogger();
-//        javax.swing.SwingUtilities.invokeLater(com.michaelb.clc.gui.Frame::new);
+        initLogger();
+        javax.swing.SwingUtilities.invokeLater(com.michaelb.clc.gui.Frame::new);
     }
 
     private static void initLogger() {
